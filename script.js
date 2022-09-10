43b9960034c7e46b5c1f7468279e92cb819d8227
forestmeichtry <fmeichtry@gmail.com>
2022-09-09 18:02:29 -0700

    Add randomize color on grid creation

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-function mouseOver() {
-    this.style.backgroundColor = 'blue';
+function mouseOver(target, color) {
+    target.style.backgroundColor = 'rgb(' +
+        color[0] + ', ' +
+        color[1] + ', ' +
+        color[2] + ')';
+}
+
+function randomColor() {
+    const color = [];
+
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+
+    return color;
 }
 
 function createGrid(size, mainContainer) {
     while (mainContainer.child) {
         mainContainer.removeChild(mainContainer.firstChild);
     }
+    const color = randomColor();
 
     for (let i = 0; i < size * size; i++) {
         let newDiv = document.createElement('div');
         newDiv.classList.add('square');
         newDiv.style.width = (800 / size) + 'px';
         newDiv.style.height = (800 / size) + 'px';
-        newDiv.addEventListener('mouseover', mouseOver);
+        newDiv.addEventListener('mouseover', function(e){
+            mouseOver(e.target, color);
+        });
         mainContainer.appendChild(newDiv);
     }
 }
